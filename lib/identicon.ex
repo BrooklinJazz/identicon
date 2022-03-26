34c9367a1ba008d7df069929984431c6f90b4a12
Brooklin <brooklinjazzmyers@gmail.com>
2022-03-26 16:45:53 -0700

    working Identicon generator first draft

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,42 +2,68 @@ defmodule Identicon do
   @moduledoc """
   Documentation for `Identicon`.
   """
+  require Integer
 
   @doc """
-  iex> Identicon.generate_start_end(0, 0)
-  {{0, 0}, {50, 50}}
-
-  iex> Identicon.generate_start_end(0, 1)
-  {{50, 0}, {100, 50}}
+    Generate and identicon for a given string
+    iex> Identicon.generate("brooklin")
+    :ok
   """
-  def generate_start_end(row, column) do
-    square_size = 50
-    start_height = row * square_size
-    end_height = start_height + square_size
-    start_width = column * square_size
-    end_width = start_width + square_size
-
-    {{start_width, start_height}, {end_width, end_height}}
+  def generate(string) do
+    string
+    |> string_to_list()
+    |> list_to_grid()
+    |> grid_to_image()
+    |> save_image("#{string}.png")
   end
 
-  def write_row(pid, color, row, row_index) do
-    row
-    |> Enum.with_index()
-    |> Enum.each(fn {_column, column_index} ->
-      {start_point, end_point} = Identicon.generate_start_end(row_index, column_index)
-      :egd.filledRectangle(pid, start_point, end_point, color)
-    end)
+  @doc """
+  Generate int list for string using md5 algorithm
+  iex> Identicon.string_to_list("brooklin")
+  [105, 174, 27, 165, 161, 201, 147, 254, 140, 232, 157, 98, 255, 210, 39, 173]
+  """
+  def string_to_list(string) do
+    :crypto.hash(:md5, string) |> :binary.bin_to_list()
   end
 
-  def int_list_from_string(input_string) when is_binary(input_string) do
-    # Choice of algorithm important?
-    :crypto.hash(:md5, input_string) |> :binary.bin_to_list()
-
-    # What output do we want?
-  end
-
-  def to_grid(list) do
-    Enum.chunk_every(list, 3, 3, :discard)
+  @doc """
+  Generate mirrored grid based on 15+ element list.
+  iex> Identicon.list_to_grid(Enum.to_list(1..15))
+  [[1, 2, 3, 2, 1], [4, 5, 6, 5, 4], [7, 8, 9, 8, 7], [10, 11, 12, 11, 10], [13, 14, 15, 14, 13]]
+  iex> Identicon.list_to_grid(Enum.to_list(1..20))
+  [[1, 2, 3, 2, 1], [4, 5, 6, 5, 4], [7, 8, 9, 8, 7], [10, 11, 12, 11, 10], [13, 14, 15, 14, 13]]
+  """
+  def list_to_grid(list) do
+    list
+    |> Enum.take(15)
+    |> Enum.chunk_every(3)
     |> Enum.map(fn [a, b, c] -> [a, b, c, b, a] end)
+  end
+
+  def grid_to_image(grid) do
+    [[r, g, b | _] | _] = grid
+    pid = :egd.create(250, 250)
+    color = :egd.color({r, g, b})
+
+    Enum.with_index(grid)
+    |> Enum.each(fn {row, row_index} ->
+      Enum.with_index(row)
+      |> Enum.each(fn {item, column_index} ->
+        if Integer.is_even(item) do
+          :egd.filledRectangle(
+            pid,
+            {column_index * 50, row_index * 50},
+            {column_index * 50 + 50, row_index * 50 + 50},
+            color
+          )
+        end
+      end)
+    end)
+
+    pid
+  end
+
+  def save_image(pid, name) do
+    :egd.render(pid) |> :egd.save(name)
   end
 end
--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -2,119 +2,19 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
-  test "we can brainstorm together" do
-    # :egd.create(100, 100)
-
-    # Draw Image (Identicon)
-
-    # Save Image (Identicon)
+  describe "generate/1" do
+    test "given brooklin generates brooklin.png" do
+      assert :ok = Identicon.generate("brooklin")
+      assert {:ok, _file} = File.read("brooklin.png")
+      assert :ok = File.rm("brooklin.png")
+    end
   end
 
-  test "can return the same list of ints for the same string" do
-    assert Identicon.int_list_from_string("Billy") == Identicon.int_list_from_string("Billy")
-    assert Enum.all?(Identicon.int_list_from_string("Billy"), &is_integer/1)
-
-    #    :crypto.hash(:md5, "string") == :crypto.hash(:md5, "string")
-    #  assert Q == :forgiven
-  end
-
-  test "can return a 5x5 list of lists" do
-    list = 1..15 |> Enum.to_list()
-
-    assert Identicon.to_grid(list) == [
-             [1, 2, 3, 2, 1],
-             [4, 5, 6, 5, 4],
-             [7, 8, 9, 8, 7],
-             [10, 11, 12, 11, 10],
-             [13, 14, 15, 14, 13]
-           ]
-  end
-
-  test "can create a 5x5 list from a int_list_from_string" do
-  end
-
-  test "can create a canvas for the identicon" do
-    # each rectangle should be50*50 to equal to250x250
-  end
-
-  test "can generate start_end location of squares" do
-    grid = Enum.to_list(1..5) |> Enum.map(fn _ -> Enum.to_list(1..5) end)
-
-    assert [
-             [
-               {{0, 0}, {50, 50}},
-               {{50, 0}, {100, 50}},
-               {{100, 0}, {150, 50}},
-               {{150, 0}, {200, 50}},
-               {{200, 0}, {250, 50}}
-             ]
-           ] =
-             Enum.with_index(grid)
-             |> Enum.map(fn {row, row_index} ->
-               row
-               |> Enum.with_index()
-               |> Enum.map(fn {column, column_index} ->
-                 Identicon.generate_start_end(row_index, column_index)
-               end)
-             end)
-  end
-
-  test "can create a 1x5 row with :egd" do
-    pid = :egd.create(250, 250)
-    red = :egd.color(:red)
-    row = [0, 1, 0, 1, 0]
-    row_index = 0
-    Identicon.write_row(pid, red, row, row_index)
-    image_binary = :egd.render(pid)
-    :egd.save(image_binary, "#{System.unique_integer()}.png")
-  end
-
-  test "can create a 5x5 image with :egd" do
-    pid = :egd.create(250, 250)
-    red = :egd.color(:red)
-    blue = :egd.color(:blue)
-
-    grid = [[0, 1, 0, 1, 0]]
-
-    Enum.with_index(grid)
-    |> Enum.map(fn {row, row_index} ->
-      row
-      |> Enum.with_index()
-      |> Enum.map(fn {column, column_index} ->
-        Identicon.generate_start_end(row_index, column_index)
-      end)
-    end)
-    |> IO.inspect()
-
-    # list2 =
-    #   Enum.map(Enum.with_index(list1), fn {item, x} ->
-    #     ## Enum.map(item, fn element ->
-    #     {{x * 50, 0}, {x * 50 + 50, 50}, red}
-    #     ## end)
-    #   end)
-
-    # list = [{{0, 0}, {50, 50}, red}, {{51, 0}, {100, 50}, blue}]
-
-    # Enum.map(list2, fn item ->
-    #   {p1, p2, color} = item
-    #   assert :ok = :egd.filledRectangle(pid, p1, p2, color)
-    # end)
-
-    # image_binary = :egd.render(pid)
-    # :egd.save(image_binary, "#{System.unique_integer()}.png")
+  describe "grid_to_image/1" do
+    test "given grid generates image" do
+      grid = Identicon.list_to_grid(Enum.to_list(1..15))
+      assert pid = Identicon.grid_to_image(grid)
+      assert is_pid(pid)
+    end
   end
 end
-
-# Present from the peanut gallery:
-# Color generator function
-# color_generator_fun = fn number ->
-#   Integer.to_string(number, 2)
-#   |> String.pad_leading(8, "0")
-#   |> String.codepoints()
-#   |> Enum.chunk_every(2)
-#   |> Enum.map(&List.to_string/1)
-#   |> Enum.map(fn part -> String.pad_leading(part, 8, "0") end)
-#   |> Enum.map(fn bin_value -> String.to_integer(bin_value, 2) end)
-#   |> Enum.map(fn number -> number * div(255, 3) end)
-#   |> List.to_tuple()
-# end
